3da3bcc895520cc47f5545761fc4f70365f9ebfa
Willie <m4ds31@gmail.com>
2022-07-23 11:44:08 +0430

    merged main into branch

--- Assets/Script/Host/Lightning.cs
+++ Assets/Script/Host/Lightning.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class lightning : MonoBehaviour
+public class Lightning : MonoBehaviour
 {
     [SerializeField]
     private float animationSpeed = 1f;
