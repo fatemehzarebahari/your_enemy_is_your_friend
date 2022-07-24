af2f54575310e7285d927eb8743bd62c16e7eb9b
YasinSHV <yshabaniv@gmail.com>
2022-07-24 20:59:05 +0430

    Pet Animation And Charge Effect

--- Assets/Scripts/Pet/PetSlam.cs
+++ Assets/Scripts/Pet/PetSlam.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PetSlam : MonoBehaviour
 {
@@ -12,13 +13,71 @@ public class PetSlam : MonoBehaviour
 	[SerializeField]
 	public float explosionStrength, range = 2, duration = 0.5f;
 
-	void Update() {
-		if ( Input.GetKeyDown(KeyCode.LeftShift)){
-			Slam();
+	[SerializeField]
+	private AudioSource explode;
+
+	private Animator animator;
+	private Camera camera;
+	private float preSize = 0f;
+	private bool chargedUp = false;
+	private GameObject camHolder;
+	[SerializeField]
+	private Image image;
+	[SerializeField]
+	private GameObject textRelease;
+
+    private void Start()
+    {
+		animator = GetComponent<Animator>();
+		camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+		camHolder = GameObject.FindGameObjectWithTag("CameraHolder");
+		preSize = camera.orthographicSize;
+    }
+    void Update() {
+		if (Input.GetKey(KeyCode.LeftShift))
+		{
+			Time.timeScale -= 1f * Time.deltaTime;
+			Time.timeScale = Mathf.Clamp(Time.timeScale, 0.35f, 1f);
+			Time.fixedDeltaTime = Time.timeScale * .02f;
+			if (Time.timeScale < 0.5f)
+				chargedUp = true;
+			camera.orthographicSize -= 4.5f * Time.deltaTime;
+			camera.orthographicSize = Mathf.Clamp(camera.orthographicSize, preSize -1.5f, preSize);
+			image.rectTransform.localScale = new Vector3(image.rectTransform.localScale.x + 20f * Time.deltaTime, image.rectTransform.localScale.y, image.rectTransform.localScale.z);
+			if (image.rectTransform.localScale.x >= 7) 
+			{
+				image.rectTransform.localScale = new Vector3(7, image.rectTransform.localScale.y, image.rectTransform.localScale.z);
+				textRelease.SetActive(true);
+			}
+
 		}
+		else
+		{
+			textRelease.SetActive(false);
+			camera.orthographicSize = preSize;
+			Time.timeScale = 1f;
+			Time.fixedDeltaTime = Time.timeScale * 0.02f;
+			image.rectTransform.localScale = new Vector3(image.rectTransform.localScale.x - 50f * Time.deltaTime, image.rectTransform.localScale.y, image.rectTransform.localScale.z);
+
+			if (image.rectTransform.localScale.x <= 0)
+				image.rectTransform.localScale = new Vector3(0, image.rectTransform.localScale.y, image.rectTransform.localScale.z);
+
+
+		}
+
+		if (chargedUp && Input.GetKeyUp(KeyCode.LeftShift))
+		{
+			animator.SetTrigger("Attack");
+			chargedUp = false;
+		}
+
+
+		
 	}
 
 	public void Slam(){
+		explode.Play();
+		StartCoroutine(Shake(0.1f, 0.1f));
 		var colliders = Physics2D.OverlapCircleAll(transform.position, range);
 
 		if (colliders.Length > 0)
@@ -38,5 +97,28 @@ public class PetSlam : MonoBehaviour
 				}
 			}
 		}
+		Time.timeScale = 1f;
+		Time.fixedDeltaTime = Time.timeScale * 0.02f;
+		camera.orthographicSize = preSize;
+	}
+
+	public IEnumerator Shake(float duration, float magnitude)
+	{
+		Vector3 origin = camHolder.transform.localPosition;
+
+		float elapsed = 0.0f;
+
+		while (elapsed < duration)
+		{
+			float x = Random.Range(-1f, 1f) * magnitude;
+			float y = Random.Range(-1f, 1f) * magnitude;
+
+			camHolder.transform.localPosition = new Vector3(x, y, origin.z);
+
+			elapsed += Time.deltaTime;
+
+			yield return null;
+		}
+		camHolder.transform.localPosition = origin;
 	}
 }
