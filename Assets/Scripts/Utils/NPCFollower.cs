bf6bfb5296b2a996ec2bed1aab09b94e191459bd
Willie <m4ds31@gmail.com>
2022-07-24 13:29:14 +0430

    Revert "today's merge"
    This reverts commit ac44df2619063e0acb8c0d255446d0be2718c124.
    
    # Conflicts:
    #	Assets/Plugins.meta
    #	Assets/Plugins/Demigiant/DOTweenPro Examples/Examples Assets/dotweenpro_logo.png.meta
    #	Assets/Prefabs/Host/Enemy.prefab
    #	Assets/Prefabs/bullet.prefab
    #	Assets/Prefabs/bullet.prefab.meta
    #	Assets/Prefabs/lineObj.prefab.meta
    #	Assets/Scenes/[Kimiya_scene_simple].unity
    #	Assets/Scenes/[Sara].unity.meta
    #	Assets/Scenes/[fatemeh_scene].unity
    #	Assets/Scripts/Host/.HostScript.cs.swp
    #	Assets/Scripts/Utils/NPCFollower.cs
    #	Assets/Sprites/Host/healthBarHost.png.meta
    #	Assets/Sprites/stripe.png.meta
    #	ProjectSettings/TagManager.asset
    #	UserSettings/EditorUserSettings.asset

--- Assets/Scripts/Utils/NPCFollower.cs
+++ Assets/Scripts/Utils/NPCFollower.cs
@@ -3,73 +3,54 @@ using UnityEngine;
 
 public class NPCFollower : MonoBehaviour
 {
-	[SerializeField]
-	public Transform player;
+    [SerializeField]
+    Transform player;
 
-	[SerializeField]
-	float speed = 30f;
-	private float currentSpeed;
+    [SerializeField]
+    float speed = 3f;
 
-	[SerializeField]
-	float range;
+    [SerializeField]
+    float firstDist;
 
-	[SerializeField]
-	private bool getPlayerAsTarget;
+    Rigidbody2D rb;
+    bool isForwarding = true;
 
-	Rigidbody2D rb;
-	bool isForwarding = true;
-
-	Vector2 player_pos, pet_pos;
-
-	float dist;
-	private void Awake()
-	{
-		rb = GetComponent<Rigidbody2D>();
-		currentSpeed = speed;
-
-		if(getPlayerAsTarget){
-			player = GameObject.FindGameObjectWithTag("Player").transform;
-		}
-	}
+    Vector2 player_pos, pet_pos;
+    
+    float dist;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
 
-	void Update()
-	{
-		if (isForwarding)
-		{
-			pet_pos = rb.position;
-			player_pos = player.position;
-			dist = (player_pos - pet_pos).magnitude;
+    void Update()
+    {
+        if (isForwarding)
+        {
+            pet_pos = rb.position;
+            player_pos = player.position;
+            dist = (player_pos - pet_pos).magnitude;
 
-			if (dist > range)
-			{
-				Vector2 aim_Vector = player_pos - pet_pos;
-				rb.velocity = aim_Vector.normalized * currentSpeed;
-			}
-			else
-			{
-				rb.velocity = Vector2.zero;
-			}
-		}
-	}
+            if (dist > firstDist)
+            {
+                Vector2 aim_Vector = player_pos - pet_pos;
+                rb.velocity = aim_Vector * speed;
+            }
+            else
+            {
+                rb.velocity = Vector2.zero;
+            }
 
-	public void startForwarding()
-	{
-		isForwarding = true;
-		resetSpeed();
-	}
+        }
+    }
+    public void startForwarding()
+    {
+        isForwarding = true;
+    }
+    public void stopForwarding()
+    {
+        isForwarding = false;
+    }
 
-	public void stopForwarding()
-	{
-		isForwarding = false;
-		rb.velocity = Vector2.zero;
-	}
-
-	public void setSpeed(float speed){
-		currentSpeed = speed;
-	}
-
-	public void resetSpeed(){
-		currentSpeed = speed;
-	}
 }
