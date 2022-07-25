c115e8ce551a8039296b4fe2be0d11c74e087525
KimiyaPoozesh <86666504+KimiyaPoozesh@users.noreply.github.com>
2022-07-25 23:07:28 +0430

    ...

--- Assets/Scripts/Bat/Bat.cs
+++ Assets/Scripts/Bat/Bat.cs
@@ -1,26 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Bat : MonoBehaviour
-{
-    public int speed = 10;
-    public Vector3 direction = Vector3.zero;
-    bool running = false;
-    void Start()
-    {
+// public class Bat : MonoBehaviour
+// {
+//     public int speed = 10;
+//     public Vector3 direction = Vector3.zero;
+//     bool running = false;
+//     public Vector3 destination;
+//     Rigidbody2D rb;
+//     void Start()
+//     {
+//         rb = GetComponent<Rigidbody2D>();   
+//     }
+
+//     void Update()
+//     {
+//             StartCoroutine(SetRandomTrajectory());
         
-    }
 
-    void Update()
-    {
-        
-        transform.position += direction*speed;
-    }
-      IEnumerator SetRandomTrajectory(){
-        yield return new WaitForSeconds(3);
-        direction.x =  Random.Range(-1f,1f);
-        direction.y = 0;        
+//         rb.velocity +=  new Vector2 (direction.x,direction.y)*Time.deltaTime; 
+//    }
+//       IEnumerator SetRandomTrajectory(){
+//         running = true;
+//         yield return new WaitForSeconds(3);
+//         direction.x =  Random.Range(-1f,1f);
+//         direction.y =  Random.Range(-1f,1f);    
+//         running = false;
+    
 
-    }
-}
+//     }
+// }
