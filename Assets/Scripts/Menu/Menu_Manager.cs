7c1bc1dcfc8d11bb0d3610a0d03e91557cf9a70d
YasinSHV <yshabaniv@gmail.com>
2022-07-26 14:45:30 +0430

    Khosgelasioon

---
+++ Assets/Scripts/Menu/Menu_Manager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Manager : MonoBehaviour
+{
+
+
+    public void OnExit() 
+    {
+        Application.Quit();
+    }
+}
